8d733ff5199f63dfc70e9bc8bcb5bb59a6766f4c
rainer-rq-koelle <rainer.koelle@eurocontrol.int>
2020-04-13 14:19:43 +0000

    first build with workflowr

---
+++ xx_rq_log.r
@@ -0,0 +1,16 @@
+# what did I do?
+library(workflowr)
+
+# Replace the example text with your information
+# this was set already when implenting the ssh key
+wflow_git_config(user.name = "Your Name", user.email = "email@domain")
+
+# /cloud/project is given in the nav part of the file viewer
+# existing=TRUE accepts other files and does not overwrite
+# workflowr acknowledges project name as "project" (not workspace name!)
+wflow_start("/cloud/project", existing = TRUE)
+
+# run an initial wflow_build() to have index, about, and license built
+wflow_build()
+# edit as appropriate and rerun
+wflow_build()
